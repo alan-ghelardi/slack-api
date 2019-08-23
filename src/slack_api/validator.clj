9d54965a9e61559ec67717718f57d2c8e1746921
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-08-23 19:49:25 -0300

    Apply a set of validators to the method-data.

--- src/slack_api/validator.clj
+++ src/slack_api/validator.clj
@@ -4,31 +4,44 @@
 
 (s/def :slack.errors/category #{:slack.errors/missing-method
                                 :slack.errors/no-such-method
-                                :slack.errors/spec-violations})
-
+                                :slack.errors/malformed-data})
 (s/def :slack.errors/message string?)
+(s/def :slack.errors/problems (s/coll-of list? :kind vector? :min-count 1))
+(s/def :slack.errors/data (s/keys :req [:slack.errors/category :slack.errors/message]
+                                  :opt [:slack.errors/problems]))
 
 (def ^:private see-available-methods "Tip: call `(slack.core/methods)` or `(slack.core/describe-methods)` to see a comprehensive list of available Slack methods.")
 
-(defn- method-is-present [{:slack/keys [method]}]
+(defn- slack-method-must-be-present [{:slack/keys [method]}]
   (when-not method
     #:slack.errors{:category :slack.errors/missing-method
                    :message
                    (format "Please, provide a `:slack/method` key containing a valid Slack method. %s" see-available-methods)}))
 
-(defn- method-exists [{:slack/keys [method]}]
+(defn- slack-method-must-exist [{:slack/keys [method]}]
   (when-not (get (web-api/get-slack-methods) method)
     #:slack.errors{:category :slack.errors/no-such-method
                    :message
                    (format "No such method `%s`. %s" method see-available-methods)}))
 
-(defn- data-satisfy-spec [method-data]
-  (let [options {:closed #{:slack/method-data}}]
-    (when-not (s/valid? :slack/method-data method-data options)
-      #:slack.errors{:category :slack.errors/spec-violations
-                     :message
-                     (format "Call `(slack-api.core/describe-method %s)` to see a detailed description about how to call this method." (:slack/method method-data))
-                     :problems (:clojure.spec.alpha/problems (s/explain-data :slack/method-data method-data options))})))
+(defn- explain-problems
+  "Returns a more succinct and friendly version of the problems returned
+  by the `explain-data` function."
+  [method-data]
+  (->> method-data
+       (s/explain-data :slack/method-data)
+       :clojure.spec.alpha/problems
+       (mapv (fn [{:keys [path pred val]}]
+               (list 'expected (symbol (name pred))
+                     'got val
+                     'at (vec (rest path)))))))
+
+(defn- data-must-satisfy-spec [method-data]
+  (when-not (s/valid? :slack/method-data method-data)
+    #:slack.errors{:category :slack.errors/malformed-data
+                   :message
+                   (format "Call `(slack-api.core/describe-method %s)` to see a detailed description about how to call this method." (:slack/method method-data))
+                   :problems (explain-problems method-data)}))
 
 (defn valid?
   "Returns true if the result represents a possibly valid method-data
@@ -36,12 +49,16 @@
   [result]
   (not (boolean (get result :slack.errors/category))))
 
-(def validation-rules
-  "Sequence of 1-arity functions that takes a method-data and returns a
+(s/fdef valid?
+  :args (s/cat :method-data :slack/method-data)
+  :ret boolean?)
+
+(def validators
+  "Sequence of 1-arity functions that take a method-data and returns a
   validation error when it is invalid."
-  [method-is-present
-   method-exists
-   data-satisfy-spec])
+  [slack-method-must-be-present
+   slack-method-must-exist
+   data-must-satisfy-spec])
 
 (defn validate-method-data
   "Applies a set of validation rules to the method-data in question in
@@ -55,8 +72,10 @@
    (some #(let [result (% method-data)]
             (when-not (valid? result)
               result))
-         validation-rules)
+         validators)
    method-data))
 
 (s/fdef validate-method-data
-  :args (s/cat :method-data :slack/method-data))
+  :args (s/cat :method-data :slack/method-data)
+  :ret (s/or :ok :slack/method-data
+             :error :slack.errors/data))
--- test/slack_api/validator_test.clj
+++ test/slack_api/validator_test.clj
@@ -3,6 +3,7 @@
             [clojure.test :refer :all]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.properties :refer [for-all]]
+            [matcher-combinators.test :refer [match?]]
             [slack-api.validator :as validator]))
 
 (deftest valid?-test
@@ -16,3 +17,18 @@
   (for-all [method-data (s/gen :slack/method-data)]
            (is (validator/valid?
                 (validator/validate-method-data method-data)))))
+
+(deftest validate-method-data-test
+  (testing "returns an error data structure for each violation below"
+    (is (match? {:slack.errors/category :slack.errors/missing-method}
+                (validator/validate-method-data {}))
+        "missing Slack method")
+
+    (is (match? {:slack.errors/category :slack.errors/no-such-method}
+                (validator/validate-method-data {:slack/method :foo}))
+        "this method doesn't exist")
+
+    (is (match? {:slack.errors/category :slack.errors/malformed-data}
+                (validator/validate-method-data {:slack/method    :api/test
+                                                 :slack.req/query {:foo 42}}))
+        "foo must be a string")))
